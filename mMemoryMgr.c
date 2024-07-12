00fcf852dde04cecd61ee80c245b73321494f988
mocul <823309460@qq.com>
2024-07-12 18:20:17 +0800

    1. prepare to add list part 2. change internal part process impl to switch function, add enum mMemoryMode_e

--- mMemoryMgr.c
+++ mMemoryMgr.c
@@ -12,16 +12,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * @brief mMgr use partInfo lower 2 bits to store <mode> and <occupy>
+ * @brief mMgr use partInfo lower 3 bits to store <mode> and <occupy>
  * @param mode the second bit of partInfo
  * @param mode[0] aligned part mode, increase speed
  * @param mode[1] reversed part mode, decrease memory usage
+ * @param mode[2] list part mode, decrease more memory usage than mode[1]
  * @param occupy the first bit of partInfo, indicates that if a part is being using
  */
 #define MMGR_GET_PART_OCCUPY(part) (((mMemoryPart_t *)(part))->partInfo & 1)
-#define MMGR_GET_PART_MODE(part) ((((mMemoryPart_t *)(part))->partInfo >> 1) & 1)
+#define MMGR_GET_PART_MODE(part) ((((mMemoryPart_t *)(part))->partInfo >> 1) & 0x03)
 #define MMGR_SET_PART_OCCUPY(part, occupy) (((mMemoryPart_t *)(part))->partInfo = ((mMemoryPart_t *)(part))->partInfo & ~1 | (occupy))
-#define MMGR_SET_PART_MODE(part, mode) (((mMemoryPart_t *)(part))->partInfo = ((mMemoryPart_t *)(part))->partInfo & ~2 | ((mode) << 1))
+#define MMGR_SET_PART_MODE(part, mode) (((mMemoryPart_t *)(part))->partInfo = ((mMemoryPart_t *)(part))->partInfo & ~0x06 | ((mode) << 1))
 
 typedef struct {
     /* namespace(data) address */
@@ -31,6 +32,13 @@ typedef struct {
     struct rb_node node;
 
 } mMemoryNs_t __attribute__((aligned(sizeof(long))));
+
+
+typedef struct {
+
+
+
+} mMemoryNsl_t __attribute__((aligned(sizeof(long))));
 
 
 typedef struct mMemoryPart {
@@ -190,7 +198,7 @@ void mMemoryMgrInit(size_t memoryPoolSize, size_t spiltParts, size_t partsMode)
         mMemoryPart_t *newPart = (mMemoryPart_t *)malloc(sizeof(mMemoryPart_t));
         
         MMGR_SET_PART_OCCUPY(newPart, 0);
-        MMGR_SET_PART_MODE(newPart, (partsMode >> i) & 1);
+        MMGR_SET_PART_MODE(newPart, (partsMode >> (i * 2)) & 0x03);
         newPart->root.rb_node = NULL;
         newPart->recycleRoot.rb_node = NULL;
         
@@ -220,112 +228,134 @@ void *mMemoryMalloc(size_t size)
         /* FIXME: this needs to be mutex, and each part should have a mutex */
         if(MMGR_GET_PART_OCCUPY(curPart))
             continue;
+
+        mMemoryNs_t *newNs = NULL;
         /* search if there have namespace->size >= size */
         namespace = searchNodeInRecycle(&curPart->recycleRoot, size);
         MMGR_SET_PART_OCCUPY(curPart, 1);
         /* if so, use recycleable space */
         if(namespace){
             ret = namespace->addr;
-            if(!MMGR_GET_PART_MODE(curPart)){
-                rb_erase(&namespace->node, &curPart->recycleRoot);
-                insertNodeInPart(&curPart->root, namespace);
-                if( (namespace->size == size) \
-                    ||(curPart->leftSpace < sizeof(mMemoryNs_t)) \
-                    ||((namespace->size - size) < sizeof(long)) )\
-                {
+            
+            switch(MMGR_GET_PART_MODE(curPart)){
+                case MMGR_MODE_ALIGN:
+                    rb_erase(&namespace->node, &curPart->recycleRoot);
+                    insertNodeInPart(&curPart->root, namespace);
+                    if( (namespace->size == size) \
+                        ||(curPart->leftSpace < sizeof(mMemoryNs_t)) \
+                        ||((namespace->size - size) < sizeof(long)) )\
+                    {
+                        MMGR_SET_PART_OCCUPY(curPart, 0);
+                        return ret;
+                    }
+
+                    newNs = (mMemoryNs_t *)curPart->addr;
+                    *(char **)&curPart->addr += sizeof(mMemoryNs_t);
+                    curPart->leftSpace -= sizeof(mMemoryNs_t);
+                    
+                    memset((void *)newNs, 0, sizeof(mMemoryNs_t));
+                    
+                    newNs->addr = namespace->addr + size;
+                    newNs->size = namespace->size - size;
+                    namespace->size = size;
+                    
+                    insertNodeInPart(&curPart->recycleRoot, newNs);
                     MMGR_SET_PART_OCCUPY(curPart, 0);
                     return ret;
-                }
+                break;
 
-                mMemoryNs_t *newNs = (mMemoryNs_t *)curPart->addr;
-                *(char **)&curPart->addr += sizeof(mMemoryNs_t);
-                curPart->leftSpace -= sizeof(mMemoryNs_t);
-                
-                memset((void *)newNs, 0, sizeof(mMemoryNs_t));
-                
-                newNs->addr = namespace->addr + size;
-                newNs->size = namespace->size - size;
-                namespace->size = size;
-                
-                insertNodeInPart(&curPart->recycleRoot, newNs);
-                MMGR_SET_PART_OCCUPY(curPart, 0);
-                return ret;
+                case MMGR_MODE_REVERSE:
+                    if(namespace->size == size || curPart->leftSpace < sizeof(mMemoryNs_t)){
+                        rb_erase(&namespace->node, &curPart->recycleRoot);
+                        insertNodeInPart(&curPart->root, namespace);
+
+                        MMGR_SET_PART_OCCUPY(curPart, 0);
+                        return namespace->addr;
+                    }
+
+                    /* if namespace size > requireSize, spilt it */
+                    rb_erase(&namespace->node, &curPart->recycleRoot);
+                    newNs = (mMemoryNs_t *)(curPart->addr + curPart->leftSpace - sizeof(mMemoryNs_t));
+                    memset((void *)newNs, 0, sizeof(mMemoryNs_t));
+                    newNs->size = size;
+                    newNs->addr = namespace->addr;
+                    insertNodeInPart(&curPart->root, newNs);
+                    namespace->addr += size;
+                    namespace->size -= size;
+                    
+                    insertNodeInPart(&curPart->recycleRoot, namespace);
+                    
+                    curPart->leftSpace -= sizeof(mMemoryNs_t);
+                    MMGR_SET_PART_OCCUPY(curPart, 0);
+                    return newNs->addr;
+                break;
+
+                default:
+
+                break;
             }
-            if(namespace->size == size || curPart->leftSpace < sizeof(mMemoryNs_t)){
-                rb_erase(&namespace->node, &curPart->recycleRoot);
-                insertNodeInPart(&curPart->root, namespace);
 
-                MMGR_SET_PART_OCCUPY(curPart, 0);
-                return namespace->addr;
-            }
-
-            /* if namespace size > requireSize, spilt it */
-            rb_erase(&namespace->node, &curPart->recycleRoot);
-            mMemoryNs_t *newNs = (mMemoryNs_t *)(curPart->addr + curPart->leftSpace - sizeof(mMemoryNs_t));
-            memset((void *)newNs, 0, sizeof(mMemoryNs_t));
-            newNs->size = size;
-            newNs->addr = namespace->addr;
-            insertNodeInPart(&curPart->root, newNs);
-            namespace->addr += size;
-            namespace->size -= size;
-            
-            insertNodeInPart(&curPart->recycleRoot, namespace);
-            
-            curPart->leftSpace -= sizeof(mMemoryNs_t);
-            MMGR_SET_PART_OCCUPY(curPart, 0);
-            return newNs->addr;
-            
         }
         /* if not, create a new rbt node */
         /* aligned mode */
-        if(!MMGR_GET_PART_MODE(curPart)){
-            uint8_t alignOffset = (sizeof(long) - size) > 0 ? sizeof(long) - size : sizeof(long) - (size % sizeof(long));
-            if(curPart->leftSpace < sizeof(mMemoryNs_t) + size + alignOffset){
-                MMGR_SET_PART_OCCUPY(curPart, 0);
-                continue;
-            }
-            mMemoryNs_t *newNs = (mMemoryNs_t *)curPart->addr;
-            
-            memset((void *)newNs, 0, sizeof(mMemoryNs_t) + size + alignOffset);
-            newNs->addr = (char *)curPart->addr + sizeof(mMemoryNs_t);
-            newNs->size = size + alignOffset;
 
-            /* union member switch to newNs->node */
-            if(!insertNodeInPart(&curPart->root, newNs)){
-                newNs->addr = 0;
-                newNs->size = 0;
-                MMGR_SET_PART_OCCUPY(curPart, 0);
-                continue;
-            }
+        switch(MMGR_GET_PART_MODE(curPart)){
+
+            case MMGR_MODE_ALIGN:
+                uint8_t alignOffset = (sizeof(long) - size) > 0 ? sizeof(long) - size : sizeof(long) - (size % sizeof(long));
+                if(curPart->leftSpace < sizeof(mMemoryNs_t) + size + alignOffset){
+                    MMGR_SET_PART_OCCUPY(curPart, 0);
+                    continue;
+                }
+                newNs = (mMemoryNs_t *)curPart->addr;
                 
-            *(char **)&curPart->addr += sizeof(mMemoryNs_t) + size + alignOffset;
-            curPart->leftSpace -= sizeof(mMemoryNs_t) + size + alignOffset;
-            MMGR_SET_PART_OCCUPY(curPart, 0);
+                memset((void *)newNs, 0, sizeof(mMemoryNs_t) + size + alignOffset);
+                newNs->addr = (char *)curPart->addr + sizeof(mMemoryNs_t);
+                newNs->size = size + alignOffset;
+
+                /* union member switch to newNs->node */
+                if(!insertNodeInPart(&curPart->root, newNs)){
+                    newNs->addr = 0;
+                    newNs->size = 0;
+                    MMGR_SET_PART_OCCUPY(curPart, 0);
+                    continue;
+                }
+                    
+                *(char **)&curPart->addr += sizeof(mMemoryNs_t) + size + alignOffset;
+                curPart->leftSpace -= sizeof(mMemoryNs_t) + size + alignOffset;
+                MMGR_SET_PART_OCCUPY(curPart, 0);
             return newNs->addr;
+            
+
+            case MMGR_MODE_REVERSE:
+                /* reverse mode */
+                if(curPart->leftSpace < sizeof(mMemoryNs_t) + size){
+                    MMGR_SET_PART_OCCUPY(curPart, 0);
+                    continue;
+                }
+                
+                newNs = (mMemoryNs_t *)(curPart->addr + curPart->leftSpace - sizeof(mMemoryNs_t));
+                memset((void *)newNs, 0, sizeof(mMemoryNs_t));
+                newNs->addr = curPart->addr;
+                newNs->size = size;
+                if(!insertNodeInPart(&curPart->root, newNs)){
+                    newNs->addr = 0;
+                    newNs->size = 0;
+                    MMGR_SET_PART_OCCUPY(curPart, 0);
+                    continue;
+                }
+                /* curPart->addr doesn't need to add sizeof(mMemoryNs_t), because all namespace headers are stored at the end of a part */
+                curPart->leftSpace -= sizeof(mMemoryNs_t) + size;
+                *(char **)&curPart->addr += size;
+                MMGR_SET_PART_OCCUPY(curPart, 0);
+            return newNs->addr;
+            
+
+            default:
+
+            break;
 
         }
-
-        /* reverse mode */
-        if(curPart->leftSpace < sizeof(mMemoryNs_t) + size){
-            MMGR_SET_PART_OCCUPY(curPart, 0);
-            continue;
-        }
-        
-        mMemoryNs_t *newNs = (mMemoryNs_t *)(curPart->addr + curPart->leftSpace - sizeof(mMemoryNs_t));
-        memset((void *)newNs, 0, sizeof(mMemoryNs_t));
-        newNs->addr = curPart->addr;
-        newNs->size = size;
-        if(!insertNodeInPart(&curPart->root, newNs)){
-            newNs->addr = 0;
-            newNs->size = 0;
-            MMGR_SET_PART_OCCUPY(curPart, 0);
-            continue;
-        }
-        /* curPart->addr doesn't need to add sizeof(mMemoryNs_t), because all namespace headers are stored at the end of a part */
-        curPart->leftSpace -= sizeof(mMemoryNs_t) + size;
-        *(char **)&curPart->addr += size;
-        MMGR_SET_PART_OCCUPY(curPart, 0);
-        return newNs->addr;
 
     }
     /* FIXME: if all parts have no left space, try to merge two parts */
